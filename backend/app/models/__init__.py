d380fad02b56292ff770b1b2e741d0268e9709aa
yjs <biz.ysdsgr@gmail.com>
2025-10-19 09:31:35 +0900

    初期化状態（空プロジェクト）

--- backend/app/models/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-SQLAlchemyモデル初期化
-"""
-
-from .user import User
-from .contact import Contact
-from .group import Group, contact_groups
-
-__all__ = ['User', 'Contact', 'Group', 'contact_groups']
